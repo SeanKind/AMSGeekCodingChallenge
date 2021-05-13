2a0874c7ffa5a246944df9d7485766e1cd94a312
Sean Coleman <seancoleman13@gmail.com>
2021-05-13 00:01:56 -0600

    shuffle method with overload

--- AMSGeekCodingChallenge/Program.cs
+++ AMSGeekCodingChallenge/Program.cs
@@ -13,7 +13,6 @@ namespace AMSGeekCodingChallenge
             Deck cardDeck = new Deck() { };
             foreach (Suit suit in Enum.GetValues(typeof(Suit)))
             {
-                
                 foreach (Face face in Enum.GetValues(typeof(Face)))
                 {
                     var card = new Card();
@@ -22,29 +21,51 @@ namespace AMSGeekCodingChallenge
                     cardDeck.deck.Add(card);
                 }
             }
-            
             return cardDeck;
         }
-        
-        public static Deck shuffle(Deck deck)
+        public static void shuffle(Deck deck)
         {
-            
-            
-            return deck;
+            var randomNumber = new Random();
+            for (int i = deck.deck.Count() - 1; i > 0; i--)
+            {
+                int j = randomNumber.Next(0, i + 1);
+                var temp = deck.deck[i];
+                deck.deck[i] = deck.deck[j];
+                deck.deck[j] = temp;
+            }
+        }
+        public static void shuffle(Deck deck, int timesToShuffle)
+        {
+            while (timesToShuffle > 0)
+            {
+                shuffle(deck);
+                timesToShuffle--;
+            }
         }
         
         
         
         static void Main(string[] args)
         {
-            //test to see if the deck will print the enum value or the actual name
             var deck = createDeck();
+            shuffle(deck, 3);
+
+
+
+
+
+
+
+
+
+
             foreach (Card card in deck.deck)
             {
                 Console.WriteLine($"{card.face} of {card.suit}");
             }
             Console.ReadLine();
 
+            //TEST FOR UNORGANIZED LIST OF CARDS
 
 
 
